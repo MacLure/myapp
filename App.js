61e61199531b3d4a72d7035d9d81e31204a21ab2
Malcolm <malcolm.maclure@gmail.com>
2019-03-30 14:46:00 -0400

    add button

--- App.js
+++ App.js
@@ -1,12 +1,20 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default class App extends React.Component {
+  pressed = (e) => {
+    console.log('pressed]')
+  }
   render() {
     return (
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
-      </View>
+        <Button
+          title="title"
+          color="#bada55"
+          onPress={this.pressed} 
+        />
+        </View>
     );
   }
 }
